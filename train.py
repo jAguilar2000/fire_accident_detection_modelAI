b9ed1295a7ac0ca7c99b9800f67f7337f8545150
DempseyPrimi <joangallegos39@gmail.com>
2025-06-24 01:06:23 -0600

    Change Model  YOLO use

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ import os
 
 # --- 1. Entrenamiento ---
 def train_model():
-    model = YOLO("yolov8n.pt")  # Modelo base (puedes usar "yolov8s.pt" para más precisión)
+    model = YOLO("yolov8m.pt")  # Modelo base (puedes usar "yolov8s.pt" para más precisión)
     results = model.train(
-        data="data.yaml",
+        data="./data.yaml",
         epochs=50,
         imgsz=640,
         batch=8,
@@ -30,7 +30,7 @@ def detect_live():
             break
         
         # Detección
-        results = model(frame, conf=0.5)  # conf = umbral de confianza
+        results = model(frame, conf=0.8)  # conf = umbral de confianza
         annotated_frame = results[0].plot()  # Dibuja las detecciones
         
         cv2.imshow("Detección de Incendios/Intrusos", annotated_frame)
@@ -40,7 +40,21 @@ def detect_live():
     cap.release()
     cv2.destroyAllWindows()
 
+def detect_from_image(image_path):
+    model = YOLO("runs/detect/fire_accident_detection/weights/best.pt")
+    image = cv2.imread(image_path)
+    
+    # Detección
+    results = model(image, conf=0.8)  # conf = umbral de confianza
+    annotated_image = results[0].plot()  # Dibuja las detecciones
+    print(f"Detecciones: {len(results[0].boxes)} objetos detectados"
+          f" en {image_path}")
+    cv2.imshow("Deteccion de Incendios/Intrusos", annotated_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 if __name__ == "__main__":
-    train_model()     # Paso 1: Entrenar
-    validate_model()  # Paso 2: Validar
+    #train_model()     # Paso 1: Entrenar
+    #validate_model()  # Paso 2: Validar
     detect_live()     # Paso 3: Probar en vivo
+    #detect_from_image("rio_portada.jpg")  # Probar con una imagen específica
